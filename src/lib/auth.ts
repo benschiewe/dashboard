b123b6668e8bcf8eba6b0b062393c1409e07dc81
Ben Schiewe <ben.schiewe@gmail.com>
2025-05-19 19:06:13 -0500

    scaffold authOptions

---
+++ src/lib/auth.ts
@@ -0,0 +1,26 @@
+import { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+
+        if (user) {
+          return user
+        } else {
+          return null
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+}

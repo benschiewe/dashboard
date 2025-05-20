63513a6f58254a66873cd42a980c147ee3f1be77
Ben Schiewe <ben.schiewe@gmail.com>
2025-05-19 19:12:13 -0500

    finish login

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,6 @@
+import { db } from "@/lib/db"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { compare } from "bcrypt"
 import { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 
@@ -9,18 +12,41 @@ export const authOptions: NextAuthOptions = {
         email: { label: "Email", type: "text" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req) {
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
-
-        if (user) {
-          return user
-        } else {
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
           return null
+        }
+
+        const existingUser = await db.user.findUnique({
+          where: { email: credentials.email },
+        })
+
+        if (!existingUser) {
+          return null
+        }
+
+        const isPasswordValid = await compare(
+          credentials.password,
+          existingUser.password,
+        )
+
+        if (!isPasswordValid) {
+          return null
+        }
+
+        return {
+          id: `${existingUser.id}`,
+          name: existingUser.name,
+          email: existingUser.email,
         }
       },
     }),
   ],
   pages: {
     signIn: "/login",
+  },
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: "jwt",
   },
 }
